843913f08173df5b69f7d336d90804b5e2228688
MusaJawad <musa.jawad@ucalgary.ca>
2023-08-30 14:22:11 -0600

    Nav Bar Created Created the Nav Bar

--- barber/src/App.js
+++ barber/src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
+      <div className = "NavBar">
+
+        <button className = "NavButton">HOME</button>
+        <button className = "NavButton">SERVICES/PRICING</button>
+        <button className = "NavButton">GALLERY</button>
+        <button className = "NavButton">APPOINTMENT/STAFF</button>
+        <button className = "NavButton">CONTACT</button>
+
+      </div>
+
+
+
     <header className="App-header">
         <h1>DSBLENDZ</h1>
         <p>Slogan</p>
